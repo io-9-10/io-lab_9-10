d5378321f44cfeb4fb604d1409b902d99f0251ae
kaxker09 <149001452+kaxker09@users.noreply.github.com>
2026-01-10 20:58:16 +0100

    Add BadCode class with nested if statements

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+public class BadCode {
+    public void bad() {
+        if (true) {
+            if (true) {
+                if (true) {
+                    System.out.println("Code smell");
+                }
+            }
+        }
+    }
+}
