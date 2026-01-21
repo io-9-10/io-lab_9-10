627f86985abe1a7bd40a8d098f186401eb2cde44
kaxker09 <149001452+kaxker09@users.noreply.github.com>
2026-01-21 21:31:24 +0100

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,15 @@
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
+
+
     public static int add(int a, int b) {
         return a + b;
     }
-    SpringApplication.run(App.class, args);
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
